9d37517570eb1775d14c1c070ea7edb53c440368
古田 精一 <s_furuta@tokyo.o-hara>
2020-09-24 09:13:17 +0900

    プロジェクトルートのstaticフォルダを設定追加、htmlファイルを更新

--- config/settings.py
+++ config/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#プロジェクトルートのstaticフォルダを追加
+import os
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
