8a1f3b3a12f170b51e828522c159342b53fdb1ff
student <student@o-hara-kashiwa.local>
2020-09-24 13:46:08 +0900

    2020/09/24 am

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from .forms import InquiryForm
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = "index.html"
+
+class InquiryView(FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
