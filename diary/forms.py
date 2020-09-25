61e0fa999edf510696b90da5cbd1eb56ab409d4d
student <student@o-hara-kashiwa.local>
2020-09-25 11:32:15 +0900

    2020/09/25 end

--- diary/forms.py
+++ diary/forms.py
@@ -1,4 +1,21 @@
 from django import forms
+from django.core.mail import EmailMessage
 
 class InquiryForm(forms.Form):
-    pass
+    name = forms.CharField(label="お名前",max_length=100)
+    mail = forms.CharField(label="メールアドレス",max_length=100)
+    title = forms.CharField(label="タイトル",max_length=100)
+    message = forms.CharField(label="メッセージ",widget=forms.Textarea)
+
+    # def __init__(self,*args,**kwargs):
+    #     super().__init__(*args,**kwargs)
+    #     self.fields['name'].widget.attrs['class'] = 'form-control.col-9'
+    #     self.fields['name'].widget.attrs['placeholder'] = 'お名前をここに入力してください'
+
+    def send_email(self):
+        subject = 'sample'
+        mail_to = ['sample@mail.com']
+        mail_from = 'sample@sample.jp'
+        body = 'テストメール'
+        mail_msg = EmailMessage(subject=subject,from_mail=mail_from,to=mail_to,body=body)
+        mail_msg.send()
--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView
 from .forms import InquiryForm
 
@@ -9,3 +10,8 @@ class IndexView(TemplateView):
 class InquiryView(FormView):
     template_name = "inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+
+    def form_vaild(self,form):
+        form.send_email()
+        return super().form_vaild(form)
