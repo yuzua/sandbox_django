208e49688b66fda357047f5042de656ff97e91af
student <student@o-hara-kashiwa.local>
2020-09-30 14:58:43 +0900

    update

--- config/urls.py
+++ config/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
 ]
 
 #開発サーバーでメディアを配信できるようにする設定
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
